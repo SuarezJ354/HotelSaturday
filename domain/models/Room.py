9e47b7679449935e60d2072eb0e6f036088ee55d
Jesus Suarez <jsuarezho@cesde.net>
2025-04-26 10:11:47 -0500

    Create Room

--- domain/models/Room.py
+++ domain/models/Room.py
@@ -10,18 +10,38 @@ class Room:
     def id(self):
         return self._id
 
+    @id.setter
+    def id(self, value):
+        self._id = value
+
     @property
     def room_number(self):
         return self._room_number
+
+    @room_number.setter
+    def room_number(self, value):
+        self._room_number = value
 
     @property
     def room_type(self):
         return self._room_type
 
+    @room_type.setter
+    def room_type(self, value):
+        self._room_type = value
+
     @property
     def price(self):
         return self._price
 
+    @price.setter
+    def price(self, value):
+        self._price = value
+
     @property
     def status(self):
         return self._status
+
+    @status.setter
+    def status(self, value):
+        self._status = value
--- view/Menu_App.py
+++ view/Menu_App.py
@@ -8,7 +8,11 @@ from application.GuestInput import GuestInput
 from domain.models.Employee import Employee
 from application.EmployeeService import EmployeeService
 from application.EmployeeInput import EmployeeInput
+from application.RoomInput import RoomInput
+from application.RoomService import RoomService
+from domain.models.Room import Room
 from repository.conexion.Conexion import Conexion
+
 
 
 class Menu_App:
@@ -23,6 +27,9 @@ class Menu_App:
         self.employee = Employee(None, None, None, None, None, None, None, None)
         self.employee_service = EmployeeService()
         self.employee_input = EmployeeInput()
+        self.room = Room (None, None, None, None, None)
+        self.room_service = RoomService()
+        self.room_input = RoomInput()
 
 
     def init_app(self):
@@ -36,10 +43,12 @@ class Menu_App:
                 print("Login")
             elif option == 2:
                 print("Registro")
-                registro = int(input("1. Empleado\n2. Huésped"))
+                registro = int(input("1. Empleado\n2. Huésped "))
                 if registro == 1:
                     self.employee_input.register(self.employee, self.db)
                 elif registro == 2:
+
+                    self.room_input.registerRoom(self.room,self.db)
                     self.guest_input.register(self.guest,self.db)
             elif option ==  3:
                 print("Saliendo...")
